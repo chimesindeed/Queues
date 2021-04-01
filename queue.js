fa57e16b0fedd83d1cd5a8c5e36c84a3f39502be
dimitri.gedda <gratitudeadore@fastmail.com>
2021-04-01 19:35:12 -0400

    initial commit regular queue

---
+++ queue.js
@@ -0,0 +1,16 @@
+// Queue
+function see(toConsole){return console.log(toConsole)}
+
+function queue(){
+
+  const queue = []
+
+  //enqueue
+  //dequeue
+  //peek
+  //length
+  //isEmpty
+}
+
+
+const q = queue();
